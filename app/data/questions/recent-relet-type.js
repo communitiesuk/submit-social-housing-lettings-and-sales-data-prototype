a3bb493fea74b68bc9cfb108c4363279763b6bcc
roobottom <roo@roodesign.co.uk>
2021-10-15 16:19:07 +0100

    adding reason for vacany

---
+++ app/data/questions/recent-relet-type.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    text: 'A social rent basis',
+    value: 'social-rent'
+  }, {
+    text: 'An affordable rent basis',
+    value: 'affordable-rent'
+  }, {
+    text: 'An intermediate rent basis',
+    value: 'intermediate-rent'
+  }, {
+    divider: 'or'
+  }, {
+    text: 'Don’t know',
+    value: 'unknown'
+  }
+]
