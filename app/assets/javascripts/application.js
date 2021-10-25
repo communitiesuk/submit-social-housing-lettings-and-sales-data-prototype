eda3960f3920f177871f09c600c2b84b337602ef
roobottom <roo@roodesign.co.uk>
2021-10-25 15:58:20 +0100

    added autocomplete to LA

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,9 +4,13 @@ import '../stylesheets/application.scss'
 // Import modules
 import './modules.js'
 import './modules/edge.js'
+import 'accessible-autocomplete'
 
 // Initiate scripts on page load
 document.addEventListener('DOMContentLoaded', () => {
   window.GOVUKFrontend.initAll()
   window.PrototypeRig.modules.start()
+  window.accessibleAutocomplete.enhanceSelectElement({
+    selectElement: document.querySelector('#autocomplete')
+  })
 })
