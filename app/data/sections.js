af5b1dc713832c4564286fd0a7ddcc99cf03ae56
roobottom <roo@roodesign.co.uk>
2021-11-30 17:32:08 +0000

    added tenant-x pages

--- app/data/sections.js
+++ app/data/sections.js
@@ -68,7 +68,12 @@ export function sections (log) {
       'lead-tenant-gender',
       'lead-tenant-nationality',
       'lead-tenant-working-situation',
-      'lead-tenant-check'
+      'lead-tenant-check',
+      'tenant-2-relationship',
+      'tenant-2-age',
+      'tenant-2-gender',
+      'tenant-2-working-situation',
+      'check-your-answers'
     ])
   }
 
