ceae921fb76bd6a371998f297d2c622501ba0922
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-12-02 14:40:26 +0000

    Progressively enhance total input to output live calculation

--- app/components/output/output.js
+++ app/components/output/output.js
@@ -1,27 +1,46 @@
 export default function () {
   this.start = (element) => {
+    const outputElement = document.createElement('output')
+
+    // Copy attributes from input element to output element
+    outputElement.classList = element.classList
+    outputElement.classList.add('app-input--output')
+    outputElement.id = `${element.id}-output`
+    outputElement.name = element.name
+    outputElement.value = element.value
+    outputElement.htmlFor = element.dataset.outputFor
+    outputElement.setAttribute(
+      'aria-describedby', element.getAttribute('aria-describedby')
+    )
+
+    // Insert output element
+    element.after(outputElement)
+
+    // Hide element used to input manually calculated total if no JS
+    element.hidden = true
+
     // List of inputs that update output are in `for` attribute
     const inputIds = []
-    element.htmlFor.forEach(input => inputIds.push(`#${input}`))
+    outputElement.htmlFor.forEach(input => inputIds.push(`#${input}`))
 
     // Get all input elements
     const inputElements = document.querySelectorAll(inputIds)
 
-    // Update output element with new value
-    const updateOutput = (inputElement, inputElements, outputElement) => {
-      inputElement.addEventListener('keyup', (e) => {
+    // Update output and original input element with new value
+    const updateOutput = (input, inputs, output, total) => {
+      input.addEventListener('keyup', (e) => {
         let sum = 0
-        inputElements.forEach((input, index, inputs) => {
+        inputs.forEach((input, index, inputs) => {
           const value = inputs[index].value.split(',').join('.').split(' ').join('')
           sum += +value
         })
 
-        outputElement.value = sum.toFixed(2)
+        output.value = total.value = sum.toFixed(2)
       })
     }
 
-    inputElements.forEach((input, index, inputs) => {
-      updateOutput(inputs[index], inputs, element)
+    inputElements.forEach((inputElement, index, inputElements) => {
+      updateOutput(inputElements[index], inputElements, outputElement, element)
     })
   }
 }
