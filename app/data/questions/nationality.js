76ad10509184c92594d10809cd4aa8674de0be19
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-12-08 16:03:27 +0000

    Use summary card for household characteristics answers

--- app/data/questions/nationality.js
+++ app/data/questions/nationality.js
@@ -1,54 +1,71 @@
 export default [{
   text: '1. UK national resident in UK',
+  answer: 'UK national resident in UK',
   value: '1'
 }, {
   text: '2. UK national returning from residence overseas',
+  answer: 'UK national returning from residence overseas',
   value: '2'
 }, {
-  text: '10. Slovenian',
-  value: '10'
-}, {
-  text: '17. Irish',
-  value: '17'
+  text: '14. Bulgarian',
+  answer: 'Bulgarian',
+  value: '14'
 }, {
   text: '16. Croatian',
+  answer: 'Croatian',
   value: '16'
 }, {
-  text: '5. Hungarian',
-  value: '5'
-}, {
   text: '3. Czech',
+  answer: 'Czech',
   value: '3'
 }, {
   text: '4. Estonian',
+  answer: 'Estonian',
   value: '4'
 }, {
+  text: '5. Hungarian',
+  answer: 'Hungarian',
+  value: '5'
+}, {
+  text: '17. Irish',
+  answer: 'Irish',
+  value: '17'
+}, {
   text: '6. Latvian',
+  answer: 'Latvian',
   value: '6'
 }, {
-  text: '9. Slovakian',
-  value: '9'
-}, {
   text: '7. Lithuanian',
+  answer: 'Lithuanian',
   value: '7'
 }, {
-  text: '14. Bulgarian',
-  value: '14'
-}, {
   text: '8. Polish',
+  answer: 'Polish',
   value: '8'
 }, {
   text: '15. Romanian',
+  answer: 'Romanian',
   value: '15'
 }, {
+  text: '9. Slovakian',
+  answer: 'Slovakian',
+  value: '9'
+}, {
+  text: '10. Slovenian',
+  answer: 'Slovenian',
+  value: '10'
+}, {
   text: '11. From another European Economic Area (EEA) country',
+  answer: 'From another European Economic Area (EEA) country',
   value: '11'
 }, {
   divider: 'or'
 }, {
   text: '12. From any other country',
+  answer: 'From any other country',
   value: '12'
 }, {
   text: '13. Tenant prefers not to say',
+  answer: 'Tenant prefers not to say',
   value: '13'
 }]
