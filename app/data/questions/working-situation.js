5919a67f173560af061258d2f96a822e07e4b830
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-01-18 15:00:53 +0000

    Question ordering

--- app/data/questions/working-situation.js
+++ app/data/questions/working-situation.js
@@ -5,20 +5,20 @@ export default [{
   text: 'Part-time – less than 30 hours a week',
   value: '2'
 }, {
+  text: 'Full-time student',
+  value: '7'
+}, {
   text: 'In government training into work, such as New Deal',
   value: '3'
 }, {
   text: 'Jobseeker',
   value: '4'
 }, {
-  text: 'Retired',
-  value: '5'
-}, {
   text: 'Not seeking work',
   value: '6'
 }, {
-  text: 'Full-time student',
-  value: '7'
+  text: 'Retired',
+  value: '5'
 }, {
   text: 'Unable to work because of long-term sickness or disability',
   value: '8'
