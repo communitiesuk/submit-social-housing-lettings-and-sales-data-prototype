9d454ccff1a9379157cd30c26bc8348e3a2fd7b2
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-02-10 17:35:03 +0000

    Implement basic filtering

--- app/data/users.js
+++ app/data/users.js
@@ -8,7 +8,7 @@ export default {
     lastActive: '2021-10-24T16:04:34'
   },
   // Data coordinator at owning organisation
-  DCOO1: {
+  DC001: {
     name: 'Delia Cross',
     email: 'data.coordinator@owning.gov.uk',
     organisationId: 'PARENT1',
@@ -17,36 +17,36 @@ export default {
     invitedById: 'ADMIN'
   },
   // Data provider at owning organisation
-  DPOO1: {
+  DP001: {
     name: 'Daisy Perkins',
     email: 'data.provider@owning.gov.uk',
     organisationId: 'PARENT1',
     role: 'provider',
-    invitedById: 'DCOO1'
+    invitedById: 'DC001'
   },
   // Data provider at managing organisation
-  DCMO1: {
+  DCM01: {
     name: 'Michael Smith',
     email: 'data.coordinator@managing.org.uk',
     organisationId: 'CHILD1',
     role: 'coordinator',
     lastActive: '2021-08-09T12:24:12',
-    invitedById: 'DCOO1'
+    invitedById: 'DC001'
   },
-  DPMO1: {
+  DPM01: {
     name: 'Sally Jones',
     email: 'data.provider@managing.org.uk',
     organisationId: 'CHILD1',
     role: 'provider',
     lastActive: '2021-09-02T13:04:18',
-    invitedById: 'DCMO1'
+    invitedById: 'DCM01'
   },
-  DPMO2: {
+  DPM02: {
     name: 'David Smith',
     email: 'david.smith@charity.org.uk',
     organisationId: 'CHILD2',
     role: 'provider',
     lastActive: '2021-10-01T11:04:18',
-    invitedById: 'DCOO1'
+    invitedById: 'DC001'
   }
 }
