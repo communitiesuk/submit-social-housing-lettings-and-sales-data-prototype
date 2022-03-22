efaa114e3e1c60193489a80380c580868107d692
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-03-22 14:29:00 +0000

    Update assets to point to new package

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,19 +4,19 @@ import '../stylesheets/application.scss'
 // Import GOV.UK Frontend
 import GOVUKFrontend from 'govuk-frontend'
 
-// Import GOV.UK Prototype Rig
-import { components as GOVUKPrototypeRig } from 'govuk-prototype-rig/rig/all.js'
+// Import GOV.UK Prototype Components
+import GOVUKPrototypeComponents from 'govuk-prototype-components'
 
 // Add app components to Rig
 import FilterLayout from '../../components/filter-layout/filter-layout.js'
 import Output from '../../components/output/output.js'
 
 // Add app components to GOVUKPrototypeRig object
-GOVUKPrototypeRig.Output = Output
-GOVUKPrototypeRig.FilterLayout = FilterLayout
+GOVUKPrototypeComponents.Output = Output
+GOVUKPrototypeComponents.FilterLayout = FilterLayout
 
 // Initiate scripts on page load
 document.addEventListener('DOMContentLoaded', () => {
   GOVUKFrontend.initAll()
-  GOVUKPrototypeRig.initAll()
+  GOVUKPrototypeComponents.initAll()
 })
