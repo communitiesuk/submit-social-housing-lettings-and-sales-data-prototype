e61c96ff50a6286e3b54f87066da479d23b6b2a1
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-06-23 16:15:45 +0100

    Allow support agents to edit who owns a scheme

--- app/datasets/seed-schemes.js
+++ app/datasets/seed-schemes.js
@@ -1,406 +1,406 @@
 export default [{
   created: '2020-11-13',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '4 Blackwood House',
   group: 'South East'
 }, {
   created: '2020-11-13',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '15-17 Defender Road',
   group: 'South West'
 }, {
   created: '2020-11-13',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: 'West View House',
   group: 'South East'
 }, {
   created: '2020-11-13',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '13 Hampage Green',
   group: 'South East'
 }, {
   created: '2020-11-13',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '3-5 Cortis Avenue',
   group: 'South East'
 }, {
   created: '2020-11-13',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '7 Weller House',
   group: 'South East'
 }, {
   created: '2020-11-13',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '9 Buriton House',
   group: 'South East'
 }, {
   created: '2020-11-13',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: 'Oxford Road',
   group: 'South West'
 }, {
   created: '2020-11-13',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: 'Victoria Road South',
   group: 'South East'
 }, {
   created: '2020-11-13',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: 'Waterloo Road',
   group: 'South East'
 }, {
   created: '2020-11-13',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '76 Fitzroy Street',
   group: 'South East'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '6 Hertford Place',
   group: 'South East'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '236 Northern Parade',
   group: 'South East'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '8 Foxcote House',
   group: 'South East'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '4 Coniston House',
   group: 'South East'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '6 Somerset Road',
   group: 'South East'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '30 Cranbury Avenue',
   group: 'South West'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: 'McCarthy House',
   group: 'South West'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: 'Sussex Street',
   group: 'South East'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '24 Northern Parade',
   group: 'South East'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '8 Eskdale House',
   group: 'South East'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '20A Dursley Crescent',
   group: 'South East'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '9 Outram Road',
   group: 'South East'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: 'Tempe Market Avenue',
   group: 'South East'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '204 Northern Parade',
   group: 'South East'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '7 Outram Road',
   group: 'South East'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '4 Melbourne House',
   group: 'South East'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '12a Finchdean Road',
   group: 'South East'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '4 Shaftesbury Road',
   group: 'South East'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: 'All Saints',
   group: 'South East'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '5 Regents Court',
   group: 'South East'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '44 Halstead Road',
   group: 'South East'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '8 Southwick House',
   group: 'South East'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: 'Portsmouth Foyer',
   group: 'South East'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '2 Tankerton Close',
   group: 'South East'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: 'Devonia Villas',
   group: 'South West'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: 'Miriam House',
   group: 'South West'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: 'Flats A-D 7 Outram Road',
   group: 'South East'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '39 Montgomerie Road',
   group: 'South East'
 }, {
   created: '2020-11-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: "143 St James' Road",
   group: 'South East'
 }, {
   created: '2020-11-17',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '12 Brisbane House',
   group: 'South East'
 }, {
   created: '2020-11-25',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '12 Ringwood House',
   group: 'South East'
 }, {
   created: '2020-11-25',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '8 Artillery Close',
   group: 'South East'
 }, {
   created: '2020-11-27',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: 'Leslie Nation Court',
   group: 'South East'
 }, {
   created: '2021-01-20',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '4 Carberry Court',
   group: 'South East'
 }, {
   created: '2021-02-26',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '4 Target Road',
   group: 'South East'
 }, {
   created: '2021-03-10',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '132 Edgbaston House',
   group: 'South East'
 }, {
   created: '2021-04-13',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '539 Northern Parade',
   group: 'South East'
 }, {
   created: '2021-04-13',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '11 Aldershot House',
   group: 'South East'
 }, {
   created: '2021-04-14',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '80 Linkenholt Way',
   group: 'South East'
 }, {
   created: '2021-04-15',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '60 Wakefords Way',
   group: 'South East'
 }, {
   created: '2021-05-10',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '52 Pickwick House',
   group: 'South East'
 }, {
   created: '2021-05-10',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '8 Chipstead House',
   group: 'South East'
 }, {
   created: '2021-05-27',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: 'Oakdene House',
   group: 'South East'
 }, {
   created: '2021-07-09',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '8 Shackleton House',
   group: 'South East'
 }, {
   created: '2021-07-23',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: 'Gibbs Terrace',
   group: 'North'
 }, {
   created: '2021-07-23',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '12 Queens Road',
   group: 'North'
 }, {
   created: '2021-07-23',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '107 London Road',
   group: 'North'
 }, {
   created: '2021-07-23',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '210 Newtown Road',
   group: 'North'
 }, {
   created: '2021-07-23',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: 'Dene Court, Dene Road',
   group: 'North'
 }, {
   created: '2021-07-23',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: 'Newbury Flats',
   group: 'North'
 }, {
   created: '2021-07-23',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: 'Prospect Court',
   group: 'North'
 }, {
   created: '2021-07-23',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '32 Cranbury Avenue',
   group: 'South West'
 }, {
   created: '2021-07-23',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '1 Foster Road',
   group: 'South East'
 }, {
   created: '2021-07-23',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: 'Oakdene Flats',
   group: 'South East'
 }, {
   created: '2021-07-23',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: 'Nile House',
   group: 'South East'
 }, {
   created: '2021-07-23',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '250 Southampton Road',
   group: 'South East'
 }, {
   created: '2021-07-23',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '101 Gosport Road',
   group: 'South East'
 }, {
   created: '2021-07-23',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: 'St Albans Rd',
   group: 'South East'
 }, {
   created: '2021-07-23',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: 'Kings Road',
   group: 'South East'
 }, {
   created: '2021-07-23',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: 'Due of Edinburgh, Cumberland Street',
   group: 'South East'
 }, {
   created: '2021-07-23',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: 'Yew House',
   group: 'South East'
 }, {
   created: '2021-07-23',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '27-29 Herbert Road',
   group: 'South East'
 }, {
   created: '2021-07-23',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: 'Acton Lodge',
   group: 'South East'
 }, {
   created: '2021-07-23',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: 'Hope House',
   group: 'South East'
 }, {
   created: '2021-07-23',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '400 Locksway Road',
   group: 'South East'
 }, {
   created: '2021-07-23',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '207 Goldsmith Avenue',
   group: 'South East'
 }, {
   created: '2021-07-23',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: 'Mill House',
   group: 'Salvation Army'
 }, {
   created: '2021-08-10',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '363 Hawthorn Crescent',
   group: 'South East'
 }, {
   created: '2021-08-10',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '6 Canberra House',
   group: 'South East'
 }, {
   created: '2021-08-10',
-  organisationId: 'OWNER',
+  ownerId: 'OWNER',
   name: '35 Cornwall Road',
   group: 'South East'
 }]
